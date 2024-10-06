d2c9a1a41d6b3e7b04cb861d146355b69a8797bb
Viktor Rura <ruravictor@gmail.com>
2024-10-06 10:48:10 +0100

    Add heroku url to allowed hosts

--- team_manager_app/settings.py
+++ team_manager_app/settings.py
@@ -17,7 +17,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "team-manager-app-ea4dc55a9401.herokuapp.com",
+]
 
 
 # Application definition
